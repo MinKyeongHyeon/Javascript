b3f8d25105c1d142d027a1d4f3e38f38e5f8937f
MinKyeonghyeon <28599@naver.com>
2025-06-30 16:37:49 +0900

    study/ 배열을 다루는 다양한 방법들 공부

---
+++ selfstudy.js
@@ -0,0 +1,17 @@
+const arr = [1, 2, 3];
+const newArr = [...arr];
+newArr[0] = 4;
+console.log(newArr);
+console.log(arr);
+console.log(arr == newArr);
+
+
+test(A: number,B : number):string {
+return A+b;
+
+}
+
+test(1, 2);
+
+const sum = (a,b) => a+b;
+sum(1,2);
